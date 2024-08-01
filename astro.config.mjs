191706c7d36d481cc9b08ffcc4ace224ed53dd59
ヘルナンデス <h@herunandesunoMacBook-Pro.local>
2024-08-01 13:50:00 +0900

    mod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(), site: 'https://astronaut.github.io',
-  base: '/my-repo',
+
   
 });
