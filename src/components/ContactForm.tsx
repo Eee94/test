b387bc63398fdeee4fcb8250dfb40f462dd58001
ヘルナンデス <h@herunandesunoMacBook-Pro.local>
2024-09-19 14:33:58 +0900

    mod

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,54 @@
+import { useForm } from 'react-hook-form'
+
+type FormValues = {
+  name: string
+  email: string
+  message: string
+}
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: 'onChange' })
+
+  const onSubmit = handleSubmit(async (data) => {
+    const formData = new FormData()
+    Object.entries(data).forEach(([key, value]) => {
+      formData.append(key, value)
+    })
+
+    await fetch('https://okna.form.newt.so/v1/K9eg399X3', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+  })
+
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name*</label>
+        <input
+          id="name"
+          {...register('name', { required: 'Name is required' })}
+          aria-describedby="error-name-required"
+        />
+        {errors?.name && (
+          <span id="error-name-required" aria-live="assertive">
+            {errors.name.message}
+          </span>
+        )}
+        <label htmlFor="email">Email</label>
+        <input id="email" {...register('email')} />
+        <label htmlFor="message">Message</label>
+        <textarea id="message" {...register('message')}></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
